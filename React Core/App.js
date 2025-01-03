e031b20e86348b85dafa87f9ba395e2c083422dc
Atulya Khandelwal <atulyakhandelwal8691@gmail.com>
2025-01-03 23:49:03 +0530

    Learn about fundamentals of JSX

--- React Core/App.js
+++ React Core/App.js
@@ -1,53 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React Element
+const heading = React.createElement("h1", { id: "heading" }, "Hello Bhai");       // React Element is an object when we render it the it become a DOM/HTML element
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);   // This will replaced not appended everything insider the root element with heading element
 
-// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React.");
+// JSX - HTML or XML like syntax in JS
 
-// console.log(heading) //object
+// JSX code transpiled before it reaches the JS engine by Parcel and Babel
 
-// root.render(heading);
+// JSX is not a valid JS syntax so we need to transpile it to JS
 
-/*
-    ReactElement(Object) => hTMl(Browser Understand)
-*/
+// JSX => React.createElement() => React Element => DOM Element
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement(
-        "div",
-        { id: "child1" },
-        [React.createElement(
-            "h1",
-            {},
-            "hi I am h1 tag of child 1"
-        ),
-            React.createElement(
-                "h2",
-                {},
-                "I am h2 tag of child 1"
-            )
-    ]
-    ),
-    React.createElement(
-        "div",
-        { id: "child2" },
-        [React.createElement(
-            "h1",
-            {},
-            "hi I am h1 tag of child 2"
-        ),
-            React.createElement(
-                "h2",
-                {},
-                "I am h2 tag of child 2"
-            )
-    ]
-    )  
-]
+// In JSX, if we want to write attributes then we need to use camelCase
+
+const jsxheading = <h1 id="jsx-heading"> Everything is fine bro! How about you?</h1>
+
+const jsxhead = (
+    <h1 id="jsx-heading">
+        Something is happening with me
+    </h1>
 )
+root.render(jsxheading)
 
-root.render(parent);
-console.log(parent)
+// React Components
+// 1. Function Components: New way of writing components
+// 2. Class Components: Old way of writing components
+
+// React Functional Component => Normal javascript function which returns a React Element
+
+
+
+const HeadingComponent2 = () => <h1 id="jsx-heading"> Everything is fine bro! How about you?</h1>
+
+const number = 1000
+const HeadingComponent = () => { 
+    return (<div>
+        <h1 id="jsx-heading"> Everything is fine bro! How about you?</h1>
+        <HeadingComponent2 />
+        <HeadingComponent2></HeadingComponent2>
+        {HeadingComponent2()}
+        {number} = {100 + 200}
+    </div> )  
+}
+root.render(<HeadingComponent />)
